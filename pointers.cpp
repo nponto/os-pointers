6dd8d7927971bbc1d2c855a56e73704826ba90c5
Noah <pont7171@stthomas.edu>
2021-02-11 20:29:24 -0600

    added data type checks

--- pointers.cpp
+++ pointers.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <sstream>
 #include <math.h>
+#include <cmath>
+
 typedef struct Student {
     int id;
     char *f_name;
@@ -34,6 +36,7 @@ int main(int argc, char **argv)
     std::cin >> student.l_name;
 
     student.n_assignments = promptInt("Please enter how many assignments were graded: ", 1, 134217728);
+    std::cout << "\n";
 
     for (int i = 0; i < student.n_assignments; i++) 
     {
@@ -43,13 +46,14 @@ int main(int argc, char **argv)
         double grade = promptDouble(message, 0.0, 10000.0);
         student.grades[i] = grade;
     }
+    std::cout << "\n";
 
      // Call `CalculateStudentAverage(???, ???)`
     calculateStudentAverage(&student, &average);
     
     // Output `average`
     std::cout << "Student: " << student.f_name << " " << student.l_name << " " << "[" << student.id << "]" << "\n";
-    std::cout << "  " << "Average Grade: " << average << "\n";
+    std::cout << "  " << "Average grade: " << average << "\n";
 
     return 0;
 }
@@ -62,9 +66,31 @@ int main(int argc, char **argv)
 int promptInt(std::string message, int min, int max)
 {
     // Code to prompt user for an int
-    int userint;
-    std::cout << message;
-    std::cin >> userint;
+    std::string tempstring;
+    int userint = -1;
+    while(1)
+    {
+        int temp = 0; // going to use this to check user response for non-digits
+        std::cout << message;
+        std::cin >> tempstring;
+        for(int i = 0; i < tempstring.length(); i++)
+        {
+            if (isdigit(tempstring[i])) // this built-in checks if the response contains a digit
+            {
+                temp++;
+            }
+            
+        }
+        if (temp == tempstring.length()) // if # of digits matches length of string then good to go
+        {
+            userint = stoi(tempstring);
+            if (userint >= min && userint <= max)
+            {
+                break;
+            }
+        }
+        std::cout << "Sorry, I cannot understand your answer" << "\n";
+    }
     return userint;
 }
 
@@ -76,9 +102,35 @@ int promptInt(std::string message, int min, int max)
 double promptDouble(std::string message, double min, double max)
 {
     // Code to prompt user for a double
-    double userdouble;
-    std::cout << message;
-    std::cin >> userdouble;
+    double userdouble = -1;
+    std::string tempstring;
+    while(1)
+    {
+        int temp = 0; // going to use this to check user response for non-digits
+        std::cout << message;
+        std::cin >> tempstring;
+        int decimals = 0;
+        for(int i = 0; i < tempstring.length(); i++)
+        {
+        if (isdigit(tempstring[i]) || tempstring[i] == '.') // this built-in checks if the response contains a digit
+            {
+                decimals++;
+                temp++;
+            }
+        }
+        if (temp == tempstring.length()) // if # of digits matches length of string then good to go
+        {
+            if (decimals = 1 || decimals == 0) // if it is a valid double (0.0 or 0)
+            {
+                userdouble = stod(tempstring);
+                if (userdouble >= min && userdouble <= max)
+                {   
+                    break;
+                }
+            }
+        }
+        std::cout << "Sorry, I cannot understand your answer" << "\n";
+    }
 
     return userdouble;
 }
